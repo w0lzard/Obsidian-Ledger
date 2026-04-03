a6d12e7fd665febbd2e507bf0da8e062d58d96cd
wolzard <kpsingj6@gmail.com>
2026-04-04 05:03:27 +0530

    Add initial configuration files and core data models for the application: sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/di/KoinInit.kt

--- sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/di/KoinInit.kt
+++ sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/di/KoinInit.kt
@@ -2,14 +2,17 @@ package com.ryuken.obsidianledger.core.di
 
 import org.koin.core.module.Module
 import org.koin.core.context.startKoin
+import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 
 fun initKoin(
-    platformModule: Module = module {}
+    platformModule: Module = module {},
+    appDeclaration: KoinAppDeclaration = {}
 ) {
     startKoin {
+        appDeclaration()
         modules(
-            platformModule,  // Android Context, iOS NSUserDefaults etc.
+            platformModule,
             coreModule,
             featureModule
         )
