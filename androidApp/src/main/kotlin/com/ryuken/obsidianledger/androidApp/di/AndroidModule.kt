ba159c74319642083a1b5ad23858407ba0d64d6e
wolzard <kpsingj6@gmail.com>
2026-04-06 21:37:22 +0530

    Add split group feature with UI components and data models: androidApp/src/main/kotlin/com/ryuken/obsidianledger/androidApp/di/AndroidModule.kt

--- androidApp/src/main/kotlin/com/ryuken/obsidianledger/androidApp/di/AndroidModule.kt
+++ androidApp/src/main/kotlin/com/ryuken/obsidianledger/androidApp/di/AndroidModule.kt
@@ -1,10 +1,13 @@
 package com.ryuken.obsidianledger.androidApp.di
 
 import com.ryuken.obsidianledger.core.database.DatabaseDriverFactory
+import com.ryuken.obsidianledger.core.preferences.AndroidAppPreferences
+import com.ryuken.obsidianledger.core.preferences.AppPreferences
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val androidModule = module {
     single { DatabaseDriverFactory(androidContext()) }
     single { SyncScheduler(androidContext()) }
+    single<AppPreferences> { AndroidAppPreferences(androidContext()) }
 }
