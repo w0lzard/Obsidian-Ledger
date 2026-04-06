3f9b2bd4969ef3dddbf5644c39d465e4353343b8
wolzard <kpsingj6@gmail.com>
2026-04-06 21:37:22 +0530

    Add split group feature with UI components and data models: sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/features/profile/ProfileIntent.kt

--- sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/features/profile/ProfileIntent.kt
+++ sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/features/profile/ProfileIntent.kt
@@ -5,6 +5,20 @@ sealed interface ProfileIntent {
     data object ExportCsv            : ProfileIntent
     data object SyncNow              : ProfileIntent
     data object SignOut               : ProfileIntent
+    
+    // Dialog Toggles
+    data class ToggleCurrencyDialog(val open: Boolean)      : ProfileIntent
+    data class ToggleThemeDialog(val open: Boolean)         : ProfileIntent
+    data class ToggleEditProfileDialog(val open: Boolean)   : ProfileIntent
+    data class ToggleChangePasswordDialog(val open: Boolean): ProfileIntent
+    data class ToggleImportDialog(val open: Boolean)        : ProfileIntent
+
+    // Actions
+    data class SetCurrency(val currency: String)            : ProfileIntent
+    data class SetTheme(val theme: String)                  : ProfileIntent
+    data class ToggleNotifications(val enabled: Boolean)    : ProfileIntent
+    data class UpdateDisplayName(val name: String)          : ProfileIntent
+    data class UpdatePassword(val pass: String)             : ProfileIntent
 }
 
 sealed interface ProfileEffect {
@@ -12,4 +26,5 @@ sealed interface ProfileEffect {
     data object SignedOut                      : ProfileEffect
     data class  Error(val message: String)    : ProfileEffect
     data object SyncComplete                   : ProfileEffect
+    data class  ShowMessage(val message: String) : ProfileEffect
 }
