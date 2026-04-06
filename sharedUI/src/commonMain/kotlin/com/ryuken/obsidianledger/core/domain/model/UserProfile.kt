23e89d9137568c1b195728d62ad7fa00f496f4c4
wolzard <kpsingj6@gmail.com>
2026-04-06 21:37:22 +0530

    Add split group feature with UI components and data models: sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/domain/model/UserProfile.kt

--- sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/domain/model/UserProfile.kt
+++ sharedUI/src/commonMain/kotlin/com/ryuken/obsidianledger/core/domain/model/UserProfile.kt
@@ -3,5 +3,6 @@ package com.ryuken.obsidianledger.core.domain.model
 data class UserProfile(
     val id          : String,
     val displayName : String,
-    val email       : String
+    val email       : String,
+    val createdAt   : String? = null
 )
