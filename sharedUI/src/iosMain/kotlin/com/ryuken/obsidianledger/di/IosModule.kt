edbea654ca6bfbc5839352b2a14dba6f83a7dce8
wolzard <kpsingj6@gmail.com>
2026-04-06 21:37:22 +0530

    Add split group feature with UI components and data models: sharedUI/src/iosMain/kotlin/com/ryuken/obsidianledger/di/IosModule.kt

--- sharedUI/src/iosMain/kotlin/com/ryuken/obsidianledger/di/IosModule.kt
+++ sharedUI/src/iosMain/kotlin/com/ryuken/obsidianledger/di/IosModule.kt
@@ -1,8 +1,11 @@
 package com.ryuken.obsidianledger.di
 
 import com.ryuken.obsidianledger.core.database.DatabaseDriverFactory
+import com.ryuken.obsidianledger.core.preferences.AppPreferences
+import com.ryuken.obsidianledger.core.preferences.IosAppPreferences
 import org.koin.dsl.module
 
 val iosModule = module {
     single { DatabaseDriverFactory() }
+    single<AppPreferences> { IosAppPreferences() }
 }
